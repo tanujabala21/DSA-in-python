5c812add17274a2156f54982537287ecae2d88fb
tanuja47 <btanuja303@gmail.com>
2026-04-06 19:24:39 +0530

    added 3sum

--- arrays/easy/medium/3sum.py
+++ arrays/easy/medium/3sum.py
@@ -23,7 +23,7 @@ class Solution(object):
                 
                     while left<right and nums[left]==nums[left-1]:
                         left+=1
-                    while left<right and nums[right]==nums[right-1]:
+                    while left<right and nums[right]==nums[right+1]:
                         right-=1
                 elif total<0:
                     left+=1
